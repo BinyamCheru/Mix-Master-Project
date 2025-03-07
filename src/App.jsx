3bc85a557b76a53fda08dda17c8b96bd3c6554b0
Bina <benjaminbinyam500@gmail.com>
2025-03-07 13:39:51 +0300

    Mix Master project setup starter file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {About, HomeLayout } from "./pages"
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+  },
+  {
+    path: "/about",
+    element: <About/>
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
